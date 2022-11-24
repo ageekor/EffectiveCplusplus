2822f4d5d99929835be3e3642d3a347292bfa921
lichengxiang01 <2928153115@qq.com>
2022-11-24 15:46:13 +0800

    modify test01

--- Unit02/test01.cpp
+++ Unit02/test01.cpp
@@ -1,31 +1,18 @@
 #include<iostream>
 using namespace std;
 
-class A {
+template<typename T>
+class NameObject {
     public:
-    static const int x = 1;
-    static void hello() {
-        cout << "hello" << endl;
-    }
-    int operator()(int a, int b) {
-        return a > b ? a : b;
-    }
+    NameObject(string& name, const T& value):name(name), value(value) {}
+    private:
+    string& name;
+    const T& value;
 };
-const int A::x;
-
-void exam() {
-    static int a = 2;
-    cout << "hi" << endl;
-    if (--a) exam();
-}
-
 int main() {
-    A* p = new(A);
-    cout << p->x << endl;
-    cout << (*p)(3, 5) << endl;
-    delete(p);
-    cout << A::x << endl;
-    A::hello();
-    
-    exam();
+    string s1 = "olddog";
+    string s2 = "newdog";
+    NameObject<int> p(s1, 1);
+    NameObject<int> q(s2, 2);
+    // p = q;
 }
